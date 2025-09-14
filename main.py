a312e4e0e8ffd8c3e0bb6a974725d23e3c996b6e
LuisGabriel3010 <luis.g.strapasson@gmail.com>
2025-09-14 17:21:04 -0300

    adicionar tarefa
    Adicionar tarefe com metodo post e função

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from typing import List
 
 app = FastAPI()
 
-class tarefa(BaseModel):
+class Tarefa(BaseModel):
     id:int
     titulo:str
     concluida: bool = False
+
+tarefas :List[Tarefa] = []
+
+@app.post("/tarefa/",response_model=Tarefa)
+def criar_tarefa(tarefa:Tarefa):
+    tarefas.append(tarefa)
+    return tarefa
+
